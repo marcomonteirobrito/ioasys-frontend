467a937d62a216ec698ea8f2d848ed90f9a308e4
Marco Monteiro <marco_monteiro@live.com>
2021-02-10 00:26:00 -0300

    Inclusão de estilo global Inclusão de cor

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,23 +1,40 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
     outline: 0;
-    box-sizing: border-box;
+    box-sizing: border-box
   }
-  
-  html, body, #root {
-    min-height: 100%;
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, border-style, #root {
+    height: 100vh;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Heebo', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
   }
 
   button {
     cursor: pointer;
   }
-
-  body {
-  -webkit-font-smoothing: antialiased !important;
-}
-
 `;
--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,6 +1,7 @@
 const Colors = {
   white: '#ffffff',
   black32: 'rgba(0, 0, 0, 0.32)',
+  buttonLabelColor: '#B22E6F',
 };
 
 export default Colors;
