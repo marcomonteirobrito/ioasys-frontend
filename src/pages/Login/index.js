009e760f4c0c139ede7b7a9430f3abb97846832d
Marco Monteiro <marco_monteiro@live.com>
2021-02-10 00:25:43 -0300

    Feat: Mudanças no componente de Login Refactor: Melhoria na estilização do componente de Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Input from '../../components/Input';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  LoginContainer,
+  HeaderContainer,
+  LogoContainer,
+} from './styles';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Container>
-      <Title />
-      <Input />
+      <LoginContainer>
+        <HeaderContainer>
+          <LogoContainer />
+          <Title>Books</Title>
+        </HeaderContainer>
+
+        <Input
+          inputTitle="Email"
+          inputType="email"
+          placeholder="Digite seu melhor e-mail"
+          onChange={(event) => setEmail(event.value)}
+          value={email}
+        />
+        <Input
+          inputTitle="Senha"
+          inputType="password"
+          placeholder="Digite sua senha"
+          onChange={(event) => setPassword(event.value)}
+          value={password}
+          button
+        />
+      </LoginContainer>
     </Container>
   );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,13 +2,39 @@ import styled from 'styled-components';
 
 import background from '../../assets/background.png';
 import Colors from '../../utils/Colors';
+import Logo from '../../assets/Logo.png';
+
+export const Container = styled.div`
+  display: flex;
+  background-image: url(${background});
+  height: 100vh;
+  background-size: cover;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 export const Title = styled.label`
   color: ${Colors.white};
-  height: 36px;
+  font-weight: 300;
+  line-height: 40px;
+  font-size: 28px;
+  margin-bottom: 48px;
 `;
-export const Container = styled.div`
-  background-image: url(${background});
 
-  width: 100%;
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 115px;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+`;
+
+export const LogoContainer = styled.div`
+  background-image: url(${Logo});
+  width: 104px;
+  height: 36px;
+  margin-right: 16px;
 `;
