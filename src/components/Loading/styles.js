6a9bb8e90616d3a1ede13b596254bba918e148d8
Marco Monteiro <marco_monteiro@live.com>
2021-02-11 00:26:50 -0300

    Refactor: Deletado componente de Login

--- src/components/Loading/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { ProgressSpinner } from 'primereact/progressspinner';
-
-export const LoadingContainer = styled(ProgressSpinner)`
-  width: 25px;
-  height: 25px;
-`;
