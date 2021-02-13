aac74475fe95e25b31c908a6cf627c06c2a31e39
Marco Monteiro <marco_monteiro@live.com>
2021-02-13 20:31:46 -0300

    Feat: Incluído cores

--- src/commons/Colors.js
+++ src/commons/Colors.js
@@ -1,7 +1,10 @@
 const Colors = {
   white: '#ffffff',
   black32: 'rgba(0, 0, 0, 0.32)',
+  textBlack: '#333333',
   buttonLabelColor: '#B22E6F',
+  backgroundErrorContainer: 'rgba(255, 255, 255, 0.4)',
+  borderCirculeLogout: 'rgba(51, 51, 51, 0.2)',
 };
 
 export default Colors;
